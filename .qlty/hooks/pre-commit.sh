0b3d6c6757736c9bd4d532b9bb8ab3c522248b36
Ben Carey <bdmc@sinemacula.co.uk>
2026-06-01 03:28:35 +0100

    chore: restore qlty-autogenerated pre-commit hook
    The .qlty/hooks scripts are generated and managed by Qlty; keep the
    pre-commit hook exactly as Qlty ships it rather than hand-editing it.

--- .qlty/hooks/pre-commit.sh
+++ .qlty/hooks/pre-commit.sh
@@ -1,6 +1,2 @@
 #!/bin/sh
-if [ -n "$GIT_INDEX_FILE" ]; then
-    qlty fmt --trigger pre-commit --index-file="$GIT_INDEX_FILE"
-else
-    qlty fmt --trigger pre-commit --index
-fi
+qlty fmt --trigger pre-commit --index-file="$GIT_INDEX_FILE"
